37e38a94cfcbe8a29358babf82f4b08cdae476e7
adriano-ferrari <irarrefonairda@gmail.com>
2024-05-12 22:21:45 -0300

    Django 5 By Example - chapter 5: Buiding a custom authentication backend

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -136,3 +136,9 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
+]
