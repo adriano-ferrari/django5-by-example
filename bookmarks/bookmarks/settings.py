1217ba24d8ac2964a6b91758079f57bac3dea3fc
adriano-ferrari <irarrefonairda@gmail.com>
2024-05-18 12:26:47 -0300

    Django 5 By Example - chapter 6: Creating a detais view for images

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rdApps
+    'django_extensions',
     # MyApps
     'bookmarks.images.apps.ImagesConfig',
 ]
@@ -122,11 +124,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_ROOT = str(BASE_DIR / 'public')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
